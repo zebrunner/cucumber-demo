fa0d99551fa600a386b92bc4592581933a1b4814
vdelendik <vadim.delendik@gmail.com>
2022-06-24 12:53:57 +0300

    sample retry failure repro steps

--- src/test/java/com/zebrunner/carina/demo/cucumber/steps/GSMArenaNewsSteps.java
+++ src/test/java/com/zebrunner/carina/demo/cucumber/steps/GSMArenaNewsSteps.java
@@ -19,12 +19,17 @@ import io.cucumber.java.en.When;
 
 
 public class GSMArenaNewsSteps extends CucumberRunner {
+    private static int i = 0;
     
     HomePage homePage = null;
     NewsPage newsPage = null;
 
     @Given("^I am on main page")
     public boolean iAmOnMainPage() {
+        if (i==0) {
+            i++;
+            Assert.fail("fail");
+        }
         homePage = new HomePage(getDriver());
         homePage.open();
         return homePage.isPageOpened();
--- src/test/resources/features/GSMArenaNews.feature
+++ src/test/resources/features/GSMArenaNews.feature
@@ -8,3 +8,9 @@ Feature: GSM Arena News testing
     Then page 'News' should be open
     And page 'News' should contains all items
 
+  @demo
+  Scenario: GSM Arena open page - passing
+    Given I am on main page
+    When I open 'News' page
+    Then page 'News' should be open
+    And page 'News' should contains all items
