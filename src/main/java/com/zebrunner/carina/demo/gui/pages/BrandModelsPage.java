9d7d84df7ecb82c160640b875dc8b261bad2038c
vdelendik <vadim.delendik@gmail.com>
2023-02-15 14:09:33 +0300

    fixed compilation failure

--- src/main/java/com/zebrunner/carina/demo/gui/pages/BrandModelsPage.java
+++ src/main/java/com/zebrunner/carina/demo/gui/pages/BrandModelsPage.java
@@ -9,12 +9,13 @@ import com.qaprosoft.carina.core.gui.AbstractPage;
 import com.zebrunner.carina.demo.gui.components.ModelItem;
 
 public class BrandModelsPage extends AbstractPage {
-    String s
+    String s = null;
     @FindBy(xpath = "//div[@id='review-body']//li")
     private List<ModelItem> models;
 
     public BrandModelsPage(WebDriver driver) {
         super(driver);
+        boolean b = s.equals("qwe");
     }
 
     public ModelInfoPage selectModel(String modelName) {
