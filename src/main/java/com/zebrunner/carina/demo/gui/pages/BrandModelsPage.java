9e7b596b795bfa6a2c63fffaee4b0e7afeb50d35
vdelendik <vadim.delendik@gmail.com>
2023-02-22 21:16:02 +0300

    Update BrandModelsPage.java

--- src/main/java/com/zebrunner/carina/demo/gui/pages/BrandModelsPage.java
+++ src/main/java/com/zebrunner/carina/demo/gui/pages/BrandModelsPage.java
@@ -9,13 +9,14 @@ import com.qaprosoft.carina.core.gui.AbstractPage;
 import com.zebrunner.carina.demo.gui.components.ModelItem;
 
 public class BrandModelsPage extends AbstractPage {
-    String s;
+    String s = "123";
     @FindBy(xpath = "//div[@id='review-body']//li")
     private List<ModelItem> models;
 
     public BrandModelsPage(WebDriver driver) {
         super(driver);
         boolean b = s.equals("qwe");
+        System.out.println(b);
     }
 
     public ModelInfoPage selectModel(String modelName) {
